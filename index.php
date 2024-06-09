3a48aa3b86c4828ca12efa0f3532b904f92cf138
ashutosh-dave <ashutoshdave927@gmail.com>
2024-06-09 12:31:53 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Marketplace</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css ">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="text-center mt-5">Marketplace</h1>
+        </div>
+    </body>
+</html>
