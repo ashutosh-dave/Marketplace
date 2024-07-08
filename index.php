e6d7833f686546d367cd5167cd5179b15ee20bdb
ashutosh-dave <ashutoshdave927@gmail.com>
2024-07-09 00:11:12 +0530

    starting from scratch

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <body>
         <div class="container">
             <h1 class="text-center mt-5">Marketplace</h1>
+            <div>
+                <a href="login.php" class="btn btn-primary">Login</a>
+                <a href="register.php" class="btn btn-primary">Register</a>
+            </div>
         </div>
     </body>
 </html>
